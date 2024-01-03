0868add65b063e5f452eab1074947d3c99db85aa
Praewpan1 <152370482+Praewpan1@users.noreply.github.com>
2024-01-03 16:12:40 +0700

    Work4
    Work4

---
+++ post.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>post</title>
+</head>
+<body>
+<h1 style="text-align: center;">Webboard PRAEWPAN</h1>
+    <hr><br>
+    <div style="text-align: center;">
+        ต้องการดูกระทู้หมายเลข  <?php echo $_GET["id"]?><br>
+       
+    
+    <table style="border: 2px solid black; width:40%" align="center">
+        <tr><td colspan="2" style="background-color: #6CD2FE;" align="left"> แสดงความคิดเห็น </td> </tr>
+        <tr><td><textarea name="message" rows="10" cols="60"></textarea></td> </tr>
+        <tr><td colspan="2"align =center><input type="submit" value="ส่งข้อความ "required></td></tr>
+
+    </table>
+    </div>
+    <br><br>
+<div style="text-align: center;"> <a href="index.html" >กลับไปหน้าหลัก</a>
+</div>
+</body>
+</html>
---
+++ verify.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Webboard PRAEWPAN</h1>
+    <hr>
+    <div style="text-align: center;">
+    เข้าสู่ระบบด้วย<br>
+    Login = <?php echo $_POST["Login"]?><br>
+    Password =  <?php echo $_POST["password"]?><br>
+    </div>
+   
+    
+</body>
+</html>
