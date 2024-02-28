bab8501d2a13c230797fc58969958709f8ff4f94
Praewpan1 <152370482+Praewpan1@users.noreply.github.com>
2024-02-28 17:42:38 +0700

    work9

--- verify.php
+++ verify.php
@@ -5,48 +5,24 @@ if (isset($_SESSION['id'])) {
     echo header("location:index.php");
     die();
 }
+$login=$_POST['Login'] ;
+$pwd=$_POST['password'];
+$conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+$sql="SELECT * FROM user where login='$login' and password=sha1('$pwd')";
+$result=$conn->query($sql);
+if($result->rowCount()==1){
+    $data=$result->fetch(PDO::FETCH_ASSOC);
+    $_SESSION['username']=$data['login'];
+    $_SESSION['role']=$data['role'];
+    $_SESSION['user_id']=$data['id'];
+    $_SESSION['id']=session_id();
+    header("location:index.php");
+    die();
+}else{
+    $_SESSION['error']="error";
+    header("location:login.php");
+    die();
 
-
+}
+$conn=null;
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Verify</title>
-</head>
-
-<body>
-    
-    
-
-        <?php
-        if ($_POST['Login'] == "admin" && $_POST['password'] == "ad1234") {
-            $_SESSION["username"] = "admin";
-            $_SESSION["role"] = "a";
-            $_SESSION["id"] = session_id();
-            header("location:index.php"); // ถ้าloginถูกไปหน้าindex admin
-            //echo "ยินดีต้อนรับคุณ ADMIN";
-        } elseif (($_POST['Login'] == "member") && ($_POST['password'] == "mem1234")) {
-            $_SESSION["username"] = "member";
-            $_SESSION["role"] = "m";
-            $_SESSION["id"] = session_id();
-            header("location:index.php");//ถ้าloginถูกไปหน้าindex member
-           // echo "ยินดีต้อนรับคุณ MEMBER";
-        } else
-            $_SESSION['error']='erroe';
-            header("location:login.php");
-            die();
-            //echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง";
-
-
-        ?>
-
-    
-    <div style="text-align: center;"> <a href="index.php">กลับไปหน้าหลัก</a></div>
-
-
-</body>
-
-</html>
